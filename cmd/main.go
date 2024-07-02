02ed34fd0fd2f356319ef8165815d03caa743edc
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2024-07-02 16:39:37 +0100

    wip: tidying cli

--- cmd/main.go
+++ cmd/main.go
@@ -11,27 +11,28 @@ import (
 func main() {
 	// TODO check messaging
 	// TODO and add examples.
+	// TODO string interpolate? providers := []string{"Bitwarden"}
 	const usageHelpMessage = `Usage: who-pwned-me [command]
 
 	Commands:
-		convert  Convert password files
+		convert  Converts a plain text password file to hashed SHA1 versions for who-pwned me to use
 			-provider string
-					Enable provider (default "")
+					Enable provider (default ""), supported providers: [Bitwarden]
 			-path string
-					Path to password file (default "")
+					Path to the exported password file to convert (default "")
 
-		compare  Compare password files
+		compare  Compare the HIBP database against your own WPM password file
 			-hibp-path string
-					Path to HIBP password file (default "")
+					Path to the HIBP password file, containing leaked SHA1 hashes (default "")
 			-wpm-path string
-					Path to WPM password file (default "")`
+					Path to the WPM password file, containing your SHA1 hashed passwords (default "")`
 	convertCommand := flag.NewFlagSet("convert", flag.ExitOnError)
-	convertProvider := convertCommand.String("provider", "", "enable")
-	convertPath := convertCommand.String("path", "", "path")
+	convertProvider := convertCommand.String("provider", "REQUIRED", "provider")
+	convertPath := convertCommand.String("path", "REQUIRED", "path")
 
 	compareCommand := flag.NewFlagSet("compare", flag.ExitOnError)
-	compareHIBPPath := compareCommand.String("hibp-path", "", "enable")
-	compareWPMPath := compareCommand.String("wpm-path", "", "path")
+	compareHIBPPath := compareCommand.String("hibp-path", "REQUIRED", "hibp-path")
+	compareWPMPath := compareCommand.String("wpm-path", "REQUIRED", "wpm-path")
 
 	if len(os.Args) < 2 {
 		fmt.Println(usageHelpMessage)
@@ -48,15 +49,10 @@ func main() {
 		// TODO, integrate
 	case "compare":
 		compareCommand.Parse(os.Args[2:])
-		fmt.Println("subcommand 'compare'")
-		fmt.Println("  HIBP path:", *compareHIBPPath)
-		fmt.Println("  WPM path:", *compareWPMPath)
-		fmt.Println("  tail:", compareCommand.Args())
-		// TODO tidy
 		passwordItems, err := compare.CompareFiles(*compareHIBPPath, *compareWPMPath)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
-			return
+			os.Exit(1)
 		}
 
 		fmt.Printf("Found: %v", passwordItems)
