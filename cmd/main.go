92da893ca4a45fc2f563271b88179951e7cc3a12
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2024-07-04 21:00:43 +0100

    refactor: tidy inputs

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -10,35 +11,33 @@ import (
 )
 
 func main() {
-	// TODO check messaging
-
 	providers := [...]string{"bitwarden"}
 	usageHelpMessage := fmt.Sprintf(`Usage: who-pwned-me [command]
 
 	Commands:
 		convert  Converts a plain text password file to hashed SHA1 versions for who-pwned me to use
-			-provider string
-					Enable provider (default ""), supported providers: %s
-			-path string
-					Path to the exported password file to convert (default "")
+			-provider
+					The provider of the password export (REQUIRED), supported providers: %s
+			-path
+					Path to the exported password file you wish to convert (REQUIRED)
 
 		compare  Compare the HIBP database against your own WPM password file
-			-hibp-path string
-					Path to the HIBP password file, containing leaked SHA1 hashes (default "")
-			-wpm-path string
-					Path to the WPM password file, containing your SHA1 hashed passwords (default "")
+			-hibp-path
+					Path to the haveibeenpwned password file, containing SHA1 hashes (REQUIRED)
+			-wpm-path
+					Path to the who-pwned-me password file, containing your SHA1 hashed passwords (REQUIRED)
 	
 	Examples:
 		who-pwned-me convert -provider bitwarden -path bitwarden.json # convert the provided bitwarden json file to a who-pwned-me file
 		who-pwned-me compare -hibp-path hibp.txt -wpm-path wpm.json # compare the who-pwned-me file with the haveibeenpwned database file
 					`, providers)
 	convertCommand := flag.NewFlagSet("convert", flag.ExitOnError)
-	convertProvider := convertCommand.String("provider", "REQUIRED", "provider")
-	convertPath := convertCommand.String("path", "REQUIRED", "path")
+	convertProvider := convertCommand.String("provider", "", "provider")
+	convertPath := convertCommand.String("path", "", "path")
 
 	compareCommand := flag.NewFlagSet("compare", flag.ExitOnError)
-	compareHIBPPath := compareCommand.String("hibp-path", "REQUIRED", "hibp-path")
-	compareWPMPath := compareCommand.String("wpm-path", "REQUIRED", "wpm-path")
+	compareHIBPPath := compareCommand.String("hibp-path", "", "hibp-path")
+	compareWPMPath := compareCommand.String("wpm-path", "", "wpm-path")
 
 	if len(os.Args) < 2 {
 		fmt.Println(usageHelpMessage)
@@ -47,14 +46,23 @@ func main() {
 
 	switch os.Args[1] {
 	case "convert":
-		// TODO check for prescence of subcommands (missing args)
 		convertCommand.Parse(os.Args[2:])
+		if *convertProvider == "" {
+			handleIfError(errors.New("no provider specified"))
+		} else if *convertPath == "" {
+			handleIfError(errors.New("no convert path specified"))
+		}
 		json, err := convert.ConvertForProvider(*convertProvider, *convertPath)
 		handleIfError(err)
 
 		fmt.Println(json) // TODO - maybe support output file too, but to stdout is fine for now.
 	case "compare":
 		compareCommand.Parse(os.Args[2:])
+		if *compareHIBPPath == "" {
+			handleIfError(errors.New("no HIBP path specified"))
+		} else if *compareWPMPath == "" {
+			handleIfError(errors.New("no WPM path specified"))
+		}
 		passwordItems, err := compare.CompareFiles(*compareHIBPPath, *compareWPMPath)
 		handleIfError(err)
 
@@ -63,7 +71,6 @@ func main() {
 		fmt.Println(usageHelpMessage)
 		os.Exit(1)
 	}
-
 }
 
 func handleIfError(err error) {
